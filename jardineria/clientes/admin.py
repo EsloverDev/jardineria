637ea6f414f3187dd98ca1fd83f430559c4f9d48
EsloverDev <edwin.grow.code@gmail.com>
2025-04-04 18:57:41 -0500

    Migraciones de clientes y productos

--- jardineria/clientes/admin.py
+++ jardineria/clientes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CategoriaCliente, Cliente, Pedido, DetallePedido
 
-# Register your models here.
+admin.site.register(Cliente)
+admin.site.register(CategoriaCliente)
+admin.site.register(Pedido)
+admin.site.register(DetallePedido)
--- jardineria/productos/admin.py
+++ jardineria/productos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Categoria, Producto, Proveedor, Inventario
 
-# Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Producto)
+admin.site.register(Proveedor)
+admin.site.register(Inventario)
+
