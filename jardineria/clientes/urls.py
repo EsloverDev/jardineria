395b5256c289d45df1b6c46a2ed6d9f1cdca7a6b
EsloverDev <edwin.grow.code@gmail.com>
2025-04-11 17:51:42 -0500

    modulo de clientes interactivo por el id

--- jardineria/clientes/urls.py
+++ jardineria/clientes/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import inicioClientes
+from .views import inicioClientes, goIndex, parametrosURL
+
 
 urlpatterns = [
-    path('home/', inicioClientes, name='inicio')
+    path('home/', inicioClientes, name='inicio'),
+    path('index', goIndex, name='index'),
+    path('cliente/<int:cliente_id>/', parametrosURL, name='cliente')
 ]
--- jardineria/clientes/views.py
+++ jardineria/clientes/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Cliente
 def inicioClientes(request):
     return HttpResponse("<h1>Hola mundo desde Clientes!</h1>")
 
+def goIndex(request):
+    data = {'usuario': 'Diana'}
+    return render(request, 'index.html', data)
+
+def parametrosURL(request, cliente_id):
+    data = Cliente.objects.get(id=cliente_id)
+    return render(request, 'cliente.html', {'cliente': data})
